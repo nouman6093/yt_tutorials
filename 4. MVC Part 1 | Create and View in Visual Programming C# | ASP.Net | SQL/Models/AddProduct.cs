a10efb6210a5c3ac3343173f9238a66aaeeef5e5
Nouman Hameed <62693237+nouman6093@users.noreply.github.com>
2025-11-16 14:42:30 +0500

    Add AddProduct model with properties

---
+++ 4. MVC Part 1 | Create and View in Visual Programming C# | ASP.Net | SQL/Models/AddProduct.cs
@@ -0,0 +1,9 @@
+namespace mvc6.Models
+{
+    public class AddProduct
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public float Price { get; set; }
+    }
+}
