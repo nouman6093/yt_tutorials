e4496ac6c73724261d9040f7b4a1830fb4e9e3f2
Nouman Hameed <62693237+nouman6093@users.noreply.github.com>
2025-11-16 14:42:04 +0500

    Add Product entity class

---
+++ 4. MVC Part 1 | Create and View in Visual Programming C# | ASP.Net | SQL/Models/Entities/Product.cs
@@ -0,0 +1,10 @@
+namespace mvc6.Models.Entities
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public float Price { get; set; }
+    }
+}
